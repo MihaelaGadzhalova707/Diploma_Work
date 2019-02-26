a87746a1f1b7c85f280441df9f8f255a7cded180
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2019-02-26 10:04:13 +0200

    registration errors

--- aqua/routes/index.js
+++ aqua/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aqua' });
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router; //allow us to access it from a different file
